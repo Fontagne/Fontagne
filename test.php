e98321218349c57be9292407e8341a1620d81aeb
Thomas Fontagne <tfontagne@la-providence.net>
2019-09-26 11:37:57 +0200

    commit1

---
+++ test.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <link href="style.css" rel="stylesheet">
+    <title>Entrainement PHP</title>
+    </head>
+    <body>
+        <h1>
+        <?php echo "Serveur Toto"; ?>
+        </h1>
+        <h2>
+            test1
+        </h2>
+    </body>        
+</html>
