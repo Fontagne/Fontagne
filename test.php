622266c148d8af467ec1465811fe8e1a295cb656
Thomas Fontagne <tfontagne@la-providence.net>
2019-09-30 14:37:38 +0200

    Commit 2

--- test.php
+++ test.php
@@ -8,7 +8,25 @@
         <?php echo "Serveur Toto"; ?>
         </h1>
         <h2>
-            test1
+          Test formulaire
         </h2>
+
+        <form action="test.php" method="GET">
+            <label>Votre chiffre</label>
+            <!-- Zone de texte -->
+            <input type="text" name="Lechiffre" />
+            <!-- bouton de validation -->
+            <input type="submit" name="Valider" value="Cliquer pour valider" />
+        </form>
+
+        <?php 
+        // Test de la variable GET LeChiffre
+        if (isset($_GET['LeChiffre'])){    
+            echo "Ma valeur est :",$_GET['LeChiffre'];
+        }else{
+            echo "Formulaire non saisie";
+            
+        }
+        ?>    
     </body>        
 </html>
